a7ec0859474b79c5cb4e5494c143bb5792957880
Fialhoalves <fialho_vinicius@hotmail.com>
2024-11-20 20:37:36 -0300

    Adicão de uma nova rota para retornar um post específico com base no ID fornecido na URL

--- server.js
+++ server.js
@@ -1,17 +1,55 @@
 import express from "express";
 
 const app = express();
-const titulo = "O Senhor dos Anéis";
-const autor ="J.R.R. Tolkien";
-const ano = 1954;
-const genero = "Fantasia"
+app.use(express.json())
+let nextId = 1; // Variável para controlar o próximo ID a ser atribuído
 
-const livro = {
-    titulo: titulo,
-    autor: autor,
-    ano: ano,
-    genero: genero
-  };
+const livros = [
+    {
+        id: nextId++,
+        titulo: "O Senhor dos Anéis",
+        autor: "J.R.R. Tolkien",
+        ano: 1954,
+        genero: "Fantasia"
+    },
+    {
+        id: nextId++,
+        titulo: "1984",
+        autor: "George Orwell",
+        ano: 1949,
+        genero: "Distopia"
+    },
+    {
+      id: nextId++,
+      titulo: "O Pequeno Príncipe",
+      autor: "Antoine de Saint-Exupéry",
+      ano: 1943,
+      genero: "Infantil"
+  },
+    // ... outros livros com IDs incrementais
+];
+
+  
+// Rota para retornar todos os livros
+app.get('/livros', (req, res) => {
+  res.status(200).json(livros);
+});
+
+function buscarLivroPorID(id){
+  return livros.findIndex((livro) =>{
+    return livro.id == Number(id);
+  })
+};
+
+app.get('/livros/:id', (req, res) => {
+  const index = buscarLivroPorID(req.params.id);
+  res.status(200).json(livros[index]);
+
+});
+
+// app.get('/livro', (req, res) => {
+//   res.status(200).send(livro);
+// });
 
 app.listen(3000, () =>{
     console.log("Servidor escutando.....");
@@ -21,6 +59,3 @@ app.get("/api", (req,res) =>{
     res.status(200).send("Boas vindas à imersão!");
 });
 
-app.get('/livro', (req, res) => {
-    res.status(200).send(livro);
-  });
